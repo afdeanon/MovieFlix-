a3b40dddc78d1912d9c29cb1a5496087e4695e72
Angeli Faith Deanon <afdeanon@gmail.com>
2020-09-05 14:15:36 -0700

    Initial Commit

---
+++ MovieFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFlix
+//
+//  Created by Angeli Faith Deanon on 9/5/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
