88eed6ab373294c48f76fe29e9ce63259d73f619
Angeli Faith Deanon <afdeanon@gmail.com>
2020-09-10 23:21:10 -0700

    I modified the app so that the user can click on the movie cell and look at the descriptions.

---
+++ MovieFlix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  MovieFlix
+//
+//  Created by Angeli Faith Deanon on 9/10/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
